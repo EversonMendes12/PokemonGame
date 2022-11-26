ef8ad1f57414746acc3efdec739c2d64b8cfa0b6
everson <eversonmendes301@gmail.com>
2022-11-26 05:29:04 -0300

    Add laço de repeição.

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String escolha;
+        boolean continuar = true;
         Player p = new Player();
 
         System.out.println(" ///////// BEM VINDO! /////////"
-                + "\n Vamos iniciar nossa aventura Pokemon."
-                + "\n\nDigite SIM para se registrar"
-                + "\nou digite MENU");
-        escolha = scan.next();
+                + "\n Vamos iniciar nossa aventura Pokemon.");
 
-        if (escolha.equals("sim") || escolha.equals("SIM")) {
+        while (continuar) {
+            System.out.println("Digite SIM para se registrar"
+                    + "\nou digite MENU");
+            escolha = scan.next();
 
-            p.registrarPlayer();
-        } else if (escolha.equals("menu") || escolha.equals("MENU")) {
-         int op;
-            System.out.println("MENU:: "
-                    + "\n1 - CATCH");
-            op = scan.nextInt();
+            if (escolha.equals("sim") || escolha.equals("SIM")) {
 
-            if (op == 1){
-                p.catchPokemon();
+                p.registrarPlayer();
+
+                System.out.println("Deseja encerrar? digite 0");
+                int laco = scan.nextInt();
+                if (laco == 0) continuar = false;
+                else continuar = true;
+
+            }
+            if (escolha.equals("menu") || escolha.equals("MENU")) {
+                int op;
+                System.out.println("MENU:: "
+                        + "\n1 - CATCH");
+                op = scan.nextInt();
+
+                if (op == 1) p.catchPokemon();
+
+
+                System.out.println("Deseja encerrar? digite 0");
+                int laco = scan.nextInt();
+                if (laco == 0) continuar = false;
+                else continuar = true;
+
             }
         }
-
     }
 
 }
